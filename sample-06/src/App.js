cc297d13fd8ef024e18de68d0145ff989dceb857
Sudhir Jadhav <sudhir.jadhav@netcracker.com>
2023-10-25 18:03:38 +0530

    Sample 06: code refactor

--- sample-06/src/App.js
+++ sample-06/src/App.js
@@ -4,18 +4,17 @@ import './App.css';
 
 import Counter from "./Counter/Counter";
 import Random from "./Random/Random";
-import DayList from "./DayList/DayList";
-import Stopwatch from "./Stopwatch/Stopwatch";
+import DayList from "./components/DayList/DayList";
+import Stopwatch from "./components/Stopwatch/Stopwatch";
+import Nav from "./components/Nav/Nav";
 
 const App = () => (
     <>
         <header className="_font _center">
             <h1>React Practice Components</h1>
         </header>
-        <main className="_font">
-            <nav>
-
-            </nav>
+        <main className="_font main-app _card">
+            <Nav />
             <section>
                 <Counter />
                 <Random />
---
+++ sample-06/src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./nav.css"
+
+const Nav = () => {
+    return (
+        <nav>
+            This is nav
+        </nav>
+    );
+};
+
+export default Nav;
